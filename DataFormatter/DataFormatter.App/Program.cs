9d5b93e08bd88be44dbacfff1be2a002f4fa16fa
Ebby Peter <ebby.learning@outlook.com>
2018-10-31 15:46:14 +1300

    Initial checkin

---
+++ DataFormatter/DataFormatter.App/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aksharapura.DataFormatter.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var sourcePath = @"D:\OneDrive\Misc\Malayalam\Raw Data\Malayalam Dict #02.txt";
+            var testPath = @"D:\Dev\Aashan\TestDataT1.txt";
+
+            var formatter = new DataFormatter();
+            formatter.LoadData(sourcePath)
+                .ParseAsDictfmt()                
+                .SaveAsCsv(testPath, "en", "ml");
+
+            Console.ReadKey();
+        }
+    }
+}
